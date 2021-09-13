b80eb1f6cd7345e7972c73d13bded6f2254739fa
Steve Bauman <steven_bauman@outlook.com>
2021-09-13 13:40:24 -0400

    Some small grammar fixes

--- docs/src/App.vue
+++ docs/src/App.vue
@@ -116,7 +116,7 @@
               class="max-w-screen-lg text-lg sm:text-2xl sm:leading-10 text-gray-400 font-medium tracking-tight mb-10 sm:mb-11"
             >
               A date range picker component for Vue.js and Tailwind CSS,
-              dependent to day.js.
+              dependent on day.js.
             </p>
             <div
               class="flex flex-wrap space-y-4 sm:space-y-0 sm:space-x-4 sm:space-x-reverse text-center"
@@ -189,7 +189,7 @@
               </div>
               <div class="mt-2">
                 <p>
-                  Litepie Datepicker use
+                  Litepie Datepicker uses
                   <a
                     href="https://tailwindcss.com"
                     class="font-semibold text-light-blue-400"
@@ -205,7 +205,7 @@
                   >
                     Day.js
                   </a>
-                  <i>under the hood</i> , You must installing before.
+                  <i>under the hood</i>. You must install them prior to using Litepie.
                 </p>
               </div>
             </div>
@@ -260,7 +260,7 @@
               </span>
             </h3>
             <div>
-              <p>Setup globally components</p>
+              <p>Setup as a global component:</p>
               <div class="rounded-xl overflow-hidden my-6">
                 <div class="bg-light-blue-500">
                   <v-prims
@@ -272,7 +272,7 @@
               </div>
             </div>
             <div>
-              <p>Setup as single components</p>
+              <p>Setup as a single component:</p>
               <div class="rounded-xl overflow-hidden my-6">
                 <div class="bg-light-blue-500">
                   <v-prims language="html" :code="defaultContent.itWorks.asSFC">
@@ -289,7 +289,7 @@
                 aria-label="Anchor"
               ></a>
               <span class="text-lg text-gray-900">
-                Tailwind CSS configuration
+                Tailwind CSS configuration:
               </span>
             </h3>
             <div class="rounded-xl overflow-hidden my-6">
