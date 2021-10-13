58fa0dc6127f209ccf842208826a7d28e6e36b37
Gregorio Hernández Caso <gregoriohc@gmail.com>
2021-10-13 17:24:22 +0200

    Fix emitShortcut for string model value

--- src/litepie-datepicker.vue
+++ src/litepie-datepicker.vue
@@ -1282,8 +1282,8 @@ export default /*#__PURE__*/ defineComponent({
               'update:modelValue',
               useToValueFromArray(
                 {
-                  previous: s,
-                  next: e
+                  previous: dayjs(s, props.formatter.date, true),
+                  next: dayjs(e, props.formatter.date, true)
                 },
                 props
               )
